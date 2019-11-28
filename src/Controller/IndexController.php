0d2a8830603f4ddf42ea46db78eb7157d450c8f1
ckolenchenko <ckolenchenko@yukon.cv.ua>
2019-11-28 13:21:23 +0200

    Roles support configurating

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -22,7 +22,29 @@ class IndexController extends ControllerCore
  */
 	public function index(Request $request):Response
 	{
-		return $this->show($request,'pages/index.twig');
+		return $this->show($request,'pages/index.twig', ['method_name' => 'Index']);
+	}
+//______________________________________________________________________________
+
+/**
+ * @Route("/dummy1", name="dummy1")
+ * @param Request $request,
+ * @return Response
+ */
+	public function dummy1(Request $request):Response
+	{
+		return $this->show($request,'pages/index.twig', ['method_name' => 'Dummy 1']);
+	}
+//______________________________________________________________________________
+
+/**
+ * @Route("/dummy2", name="dummy2")
+ * @param Request $request,
+ * @return Response
+ */
+	public function dummy2(Request $request):Response
+	{
+		return $this->show($request,'pages/index.twig', ['method_name' => 'Dummy 2']);
 	}
 //______________________________________________________________________________
 
