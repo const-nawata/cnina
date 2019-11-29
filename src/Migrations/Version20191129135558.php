d77cc7783f809389a27b86c1eba597f498de1486
ckolenchenko <ckolenchenko@yukon.cv.ua>
2019-11-29 16:03:42 +0200

    User entity expanding (restore after rollback).

---
+++ src/Migrations/Version20191129135558.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191129135558 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users ADD firstname VARCHAR(50) DEFAULT NULL, ADD surname VARCHAR(50) DEFAULT NULL, ADD address VARCHAR(255) DEFAULT NULL, ADD phone VARCHAR(50) DEFAULT NULL, ADD postcode VARCHAR(50) DEFAULT NULL, ADD mail_addr VARCHAR(100) DEFAULT NULL, ADD confirmed TINYINT(1) DEFAULT \'0\' NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users DROP firstname, DROP surname, DROP address, DROP phone, DROP postcode, DROP mail_addr, DROP confirmed');
+    }
+}
