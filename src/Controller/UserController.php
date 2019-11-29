59d2cefc9c32e80a9a1a3ea51271a234f1cb686d
ckolenchenko <ckolenchenko@yukon.cv.ua>
2019-11-29 16:22:58 +0200

    Login form applying.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,30 +3,33 @@
 namespace App\Controller;
 
 use Exception;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class UserController extends AbstractController
+class UserController extends ControllerCore
 {
 	/**
 	 * @Route("/login", name="app_login")
+	 * @param Request $request
 	 * @param AuthenticationUtils $authenticationUtils
 	 * @return Response
 	 */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('index');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+		return $this->show($request,'pages/user/login-form.twig', ['last_username' => $lastUsername, 'error' => $error] );
     }
 
 	/**
