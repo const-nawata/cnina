266138972190be75b0abc5c85d12a0f221ebf158
ckolenchenko <ckolenchenko@yukon.cv.ua>
2019-12-11 16:37:30 +0200

    Set columns as custom parameter.

--- src/Repository/CurrencyRepository.php
+++ src/Repository/CurrencyRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\Currency;
-
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -18,14 +17,14 @@ use Psr\Log\LoggerInterface;
 class CurrencyRepository extends CoreRepository
 {
 
-    public function __construct( ManagerRegistry $registry, LoggerInterface $logger, PaginatorInterface $paginator )
-    {
-		$this->fields	= [
-			'name'	=> ['searchable' => true ],
-			'ratio'	=> ['searchable' => true]
+	public function __construct(ManagerRegistry $registry, LoggerInterface $logger, PaginatorInterface $paginator)
+	{
+		$this->fields = [
+			'name' => ['searchable' => true],
+			'ratio' => ['searchable' => true]
 		];
-		parent::__construct( $registry, Currency::class, $logger, $paginator );
-    }
+		parent::__construct($registry, Currency::class, $logger, $paginator);
+	}
 //______________________________________________________________________________
 
 	/**
@@ -34,19 +33,18 @@ class CurrencyRepository extends CoreRepository
 	 * @param string $search
 	 * @return PaginationInterface
 	 */
-	public function getPaginator( $page, $limit, $search='' ): PaginationInterface
+	public function getPaginator($page, $limit, $search = ''): PaginationInterface
 	{
-		$query = $this->getPagerQuery( $search );
+		$pagination = $this->paginator->paginate($this->getPagerQuery($search), $page, $limit);
 
-		$pagination = $this->paginator->paginate( $query, $page, $limit );
-
-		//TODO:Maybe it is better to use Pagination->options
-		$pagination->columns	= [
-			['field' => 'name',		'title' => 'form.denomination', 	'sortable' => true,	'css' => '' ],
-			['field' => 'ratio',	'title' => 'form.ratio-currency',	'sortable' => true,	'css' => 'number-list-sell' ],
-			['field' => 'sample',	'title' => 'title.sample',			'sortable' => false,'css' => 'number-list-sell' ]
-		];
-
+		$pagination->setCustomParameters([
+			'size' => 'small',
+			'columns' => [
+				['field' => 'name', 'title' => 'form.denomination', 'sortable' => true, 'css' => ''],
+				['field' => 'ratio', 'title' => 'form.ratio-currency', 'sortable' => true, 'css' => 'number-list-sell'],
+				['field' => 'sample', 'title' => 'title.sample', 'sortable' => false, 'css' => 'number-list-sell']
+			]
+		]);
 		return $pagination;
 	}
 
@@ -54,17 +52,17 @@ class CurrencyRepository extends CoreRepository
 	 * @param integer $id
 	 * @return array: Currency data
 	 */
-	public function getFormData( $id=0 ): array
+	public function getFormData($id = 0): array
 	{
-		if( $id > 0){
+		if ($id > 0) {
 			$entity = $this->find($id);
-		}else{
+		} else {
 			$entity = new Currency();
 			$entity->setIsAfterPos(true);
 		}
 
 		return [
-			'entity'	=> $entity
+			'entity' => $entity
 		];
 	}
 //______________________________________________________________________________
@@ -74,52 +72,53 @@ class CurrencyRepository extends CoreRepository
 	 * @throws \Doctrine\ORM\ORMException
 	 * @throws \Doctrine\ORM\OptimisticLockException
 	 */
-	public function saveFormData( array $post ): void
+	public function saveFormData(array $post): void
 	{
-		$entity	= ( $post['id'] > 0 )
-			? $this->find( $post['id'] )
+		$entity = ($post['id'] > 0)
+			? $this->find($post['id'])
 			: new Currency();
 
-		$post['ratio']	= str_replace(',', '.', $post['ratio']);
+		$post['ratio'] = str_replace(',', '.', $post['ratio']);
 
 		$entity->setName($post['name']);
 		$entity->setSymbol($post['symbol']);
 		$entity->setRatio($post['ratio']);
 		$entity->setIsAfterPos((bool)$post['isAfterPos']);
 
-		$this->_em->persist( $entity );
+		$this->_em->persist($entity);
 		$this->_em->flush();
 	}
+
 //______________________________________________________________________________
 
-	public function getDefault( $ratio = 1): array
+	public function getDefault($ratio = 1): array
 	{
 		$qb = $this->createQueryBuilder('c')
 			->andWhere('c.ratio = :ratio')
-			->setParameter('ratio', $ratio )
+			->setParameter('ratio', $ratio)
 			->getQuery();
 
-		$list	= $qb->execute();
-		if( count($list) > 0 ){
-			$currency	= $list[0];
+		$list = $qb->execute();
+		if (count($list) > 0) {
+			$currency = $list[0];
 			return [
-				'id'	=> $currency->getId(),
-				'name'	=> $currency->getName()
+				'id' => $currency->getId(),
+				'name' => $currency->getName()
 			];
 		}
 
-		$list	= $this->findAll();
-		if( count($list) > 0 ){
-			$currency	= $list[0];
+		$list = $this->findAll();
+		if (count($list) > 0) {
+			$currency = $list[0];
 			return [
-				'id'	=> $currency->getId(),
-				'name'	=> $currency->getName()
+				'id' => $currency->getId(),
+				'name' => $currency->getName()
 			];
 		}
 
 		return [
-			'id'	=> 0,
-			'name'	=> 'Undefined'
+			'id' => 0,
+			'name' => 'Undefined'
 		];
 	}
 //______________________________________________________________________________
