f1ec54365fb0e69cb37d406215c3697e95df9bc2
ckolenchenko <ckolenchenko@yukon.cv.ua>
2020-01-29 11:51:19 +0200

    Dashboard security. Translation for login exceptions.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -19,6 +19,9 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
@@ -28,13 +31,15 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     private $urlGenerator;
     private $csrfTokenManager;
     private $passwordEncoder;
+    private $translator;
 
-    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder, TranslatorInterface $translator)
     {
-        $this->entityManager = $entityManager;
-        $this->urlGenerator = $urlGenerator;
-        $this->csrfTokenManager = $csrfTokenManager;
-        $this->passwordEncoder = $passwordEncoder;
+        $this->entityManager	= $entityManager;
+        $this->urlGenerator		= $urlGenerator;
+        $this->csrfTokenManager	= $csrfTokenManager;
+        $this->passwordEncoder	= $passwordEncoder;
+        $this->translator		= $translator;
     }
 
     public function supports(Request $request)
@@ -70,11 +75,11 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            throw new CustomUserMessageAuthenticationException($this->translator->trans('message.no-account',[],'prompts'));
         }
 
 		if( !$user->getConfirmed() ){
-			throw new CustomUserMessageAuthenticationException("Access denied. User's account wan not confirmed.");
+			throw new CustomUserMessageAuthenticationException($this->translator->trans('message.unconfirmed-acc',[],'prompts'));
 		}
 
         return $user;
